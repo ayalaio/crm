0d68923abf9e5c49591a5abf1230a2ffc93e7297
ec2 <ec2@amazon>
2017-09-20 05:08:29 +0000

    Add db power

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source 'https://rubygems.org'
 
-gem 'sqlite3'
 gem 'rake'
 gem 'sinatra'
 gem 'sinatra-activerecord'
-gem 'activerecord', "3.2.17"
+gem 'activerecord', "3.2.18"
 gem 'pry'
 gem 'pry-byebug'
+gem 'mysql2','0.3.20'
--- db/structure.sql
+++ db/structure.sql
@@ -1,6 +1,15 @@
-CREATE TABLE "schema_migrations" ("version" varchar(255) NOT NULL);
-CREATE TABLE "users" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
-CREATE UNIQUE INDEX "unique_schema_migrations" ON "schema_migrations" ("version");
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 INSERT INTO schema_migrations (version) VALUES ('20140415201712');
 
 INSERT INTO schema_migrations (version) VALUES ('20140415204542');
